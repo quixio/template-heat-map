4730c3113b5b4987f3c11eb1c1a5d7c444253e75
Steve Rosam <srosam+quix-demos@gmail.com>
2024-05-01 17:28:57 +0000

    Edited 'main.py'

--- StarCounter/main.py
+++ StarCounter/main.py
@@ -19,13 +19,11 @@ sdf = app.dataframe(input_topic)
 
 def stars(row):
 
-    def get_stars(owner, repo):
-        url = f"https://api.github.com/repos{row['href']}"
-        response = requests.get(url)
-        data = response.json()
-        return data['stargazers_count']
 
-    stars = get_stars('dpkp', 'kafka-python')
+    url = f"https://api.github.com/repos{row['href']}"
+    response = requests.get(url)
+    data = response.json()
+    stars = data['stargazers_count']
     print(stars)
 
 sdf = sdf.update(stars)
