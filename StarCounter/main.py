e2ab56125faba69bfe517417d18c921b93a8a87a
Steve Rosam <srosam+quix-demos@gmail.com>
2024-05-01 17:28:21 +0000

    Edited 'main.py'

--- StarCounter/main.py
+++ StarCounter/main.py
@@ -20,7 +20,7 @@ sdf = app.dataframe(input_topic)
 def stars(row):
 
     def get_stars(owner, repo):
-        url = f"https://api.github.com/repos/{row['']}"
+        url = f"https://api.github.com/repos{row['href']}"
         response = requests.get(url)
         data = response.json()
         return data['stargazers_count']
